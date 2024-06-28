07a358a4cb36cb4ab3dbc926544a9af82b0574a9
mlvenkey <mlvenkateswra9@gmail.com>
2024-06-28 19:23:47 +0000

    updated string java

--- stringutils.java
+++ stringutils.java
@@ -2,6 +2,23 @@ Hello this is spring booting updated values can changed java scripted
 
 
 
-
-
+JOE
+How’d you do that?
+APRIL
+There was a cut - from the hallway
+to the capitol. What’d you want to
+do - ride in a cab?
+JOE
+Dialogue margins.
+APRIL
+Left, 2.5 inches. Right, 2.5
+inches. Of course, you can cheat
+those a tad.
+JOE
+So, you have about 3.5 inches for
+each line of dialogue?
+APRIL
+You’ve got it. And you can sneak
+out another character or two to the
+right and no one will hold it 
 
