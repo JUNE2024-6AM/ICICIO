58a95e8b936783c3b3263a45e9a31e317998585e
mlvenkey <mlvenkateswra9@gmail.com>
2024-06-28 19:13:30 +0000

    some update

--- diploy.sh
+++ diploy.sh
@@ -1,2 +1,30 @@
 Hello this file contains  diplayment creations and always changes the values to be created beavrages
 
+
+remain attached to description. A
+single line of dialogue is pushed
+to the following page. A long
+dialogue passage would be split -
+but I’ll get to that later.
+JOE
+What about fonts?
+APRIL
+Courier, 12-point, 10-pitch. Make
+sure it’s a non-proportional
+version of Courier and avoid
+Courier New unless you want your
+script to be about 10% longer.
+JOE
+What about bold-face or italics? Or
+a cool font like Garamond? I love
+to jazz up my scripts.
+APRIL
+Mostly, you should steer clear of
+bold and italics. Definitely no
+Garamond, no Helvetica, no Times
+Roman. Stick with Courier. That’s
+the industry standard.
+JOE
+Ah . . . we’re talking about
+industry standards.
+Suddenly, Joe bolts from behind the podium
